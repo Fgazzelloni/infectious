c0fbd748c5c4ccff6f687311d707b4c0fb4c7793
Federica Gazzelloni <docksbox@pm.me>
2022-12-16 10:23:46 +0100

    Initial commit

---
+++ R/notes_on_building.R
@@ -0,0 +1,10 @@
+# info on how to setup this project:
+
+# 
+# quarto first:
+# https://quarto.org/docs/publishing/github-pages.html
+# create a new project on a new directory
+quarto book project
+
+# github later:
+# source: https://happygitwithr.com/existing-github-last.html
