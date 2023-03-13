9c06faf5a0b4781f8f22ccfd7ed74395a2c14d5b
Federica Gazzelloni <docksbox@pm.me>
2023-03-13 23:20:24 +0100

    adding package documentation

---
+++ inst/building_blocks/add_data.R
@@ -0,0 +1,40 @@
+#############################################
+# building blocks of the package
+#############################################
+
+# ADD DATA TO PACKAGE
+# set the data from the original source and tidy appropriately
+usethis::use_data_raw()
+# it creates a .R script where to put the steps for get the data ready
+
+# once data are ready
+usethis::use_data(yourdata)
+
+# creates the R folder where the .R scripts of data documentation is located
+usethis::use_r("yourdataset")
+# read how to document your work
+vignette("rd-other") # for datasets
+vignette("rd")
+
+devtools::document()
+devtools::load_all(".")
+
+#############################################
+# ADD PACKAGE INFO 
+# so the ? works
+usethis::use_package_doc()
+devtools::document()
+
+
+# ADD DATA PACKAGE INFO 
+usethis::use_r("yourdataset")
+devtools::document()
+
+
+# when filling the data .R script for explaining what's inside the dataset
+# a specific structure needs to be used (see examples)
+
+
+
+
+
